5a15634ac9a7130fe72d4da6a105026b436d939f
최성근 <ccc3132@naver.com>
2016-11-11 19:44:14 +0900

    2번 드디어 완성하다 만

--- app/src/main/java/com/example/a403/projectcd/MainActivity.java
+++ app/src/main/java/com/example/a403/projectcd/MainActivity.java
@@ -4,13 +4,15 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.CompoundButton;
 import android.widget.TextView;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
     Button b1;
     TextView t1;
-
+    CheckBox c1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -28,7 +30,19 @@ public class MainActivity extends AppCompatActivity {
                 Toast.makeText(getApplicationContext(), "Hello World", Toast.LENGTH_SHORT).show();
             }
         });
+       c1 = (CheckBox) findViewById(R.id.checkBox);
+        c1.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            public  void onCheckedChanged(CompoundButton button,boolean isChecked){
+            int btnDrawable = android.R.drawable.checkbox_off_background;
 
+                    if(isChecked)
+                    {
+                        btnDrawable = android.R.drawable.checkbox_on_background;
+                    }
+
+
+            }
+        });
     }
 }
 
