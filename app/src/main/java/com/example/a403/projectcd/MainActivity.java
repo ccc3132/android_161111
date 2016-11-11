f0c23e68ae369f9c82731969c08e430b9af16f6d
최성근 <ccc3132@naver.com>
2016-11-11 19:16:48 +0900

    1번 드디어 완성

--- app/src/main/java/com/example/a403/projectcd/MainActivity.java
+++ app/src/main/java/com/example/a403/projectcd/MainActivity.java
@@ -2,12 +2,35 @@ package com.example.a403.projectcd;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+    Button b1;
+    TextView t1;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        b1 = (Button) findViewById(R.id.button);
+        b1.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                Toast.makeText(getApplicationContext(), "안녕", Toast.LENGTH_SHORT).show();
+            }
+        });
+        t1 = (TextView) findViewById(R.id.textview);
+        t1.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                Toast.makeText(getApplicationContext(), "Hello World", Toast.LENGTH_SHORT).show();
+            }
+        });
+
     }
 }
+
+
+
